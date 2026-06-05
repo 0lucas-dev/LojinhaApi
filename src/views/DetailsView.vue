2fb38113a092a1516553a5576a89f967adc445e0
lluca-s <lucasvitorweb124@gmail.com>
2026-06-05 11:23:55 -0300

    Primeiro commit

---
+++ src/views/DetailsView.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+// import { useRoute } from 'vue-router';
+// import axios from 'axios';
+// import { capitalize } from 'vue';
+
+export default {
+  name: 'DetailsView',
+}
+
+</script>
+
+<template>
+  <h1>Ola</h1>
+</template>
---
+++ src/views/ListView.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+// import axios from 'axios';
+import ProdutoCard from '../components/produto/ProdutoCard.vue';
+
+export default {
+  name: 'ListView',
+  components: {
+    ProdutoCard
+  },
+
+}
+</script>
+
+<template>
+  <div>
+    <ProdutoCard ></ProdutoCard>
+  </div>
+</template>
